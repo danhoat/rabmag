58eaef0025c66f784d3e6c1de8e9ff014d53a04e
danngyw <danng@youngworld.vn>
2015-06-05 00:43:08 +0700

    update

--- footer.php
+++ footer.php
@@ -3,15 +3,15 @@
         <div class="row footer-top">
             <div class="col-md-6">
                 <div class="entry-footer-widget">
-                    <aside class="widget widget_black_studio_tinymce" id="black-studio-tinymce-2"><h3 class="title-widget">CÔNG TY NỘI THẤT NAM HÀ PHÁT</h3> <div class="entry-post"><div class="textwidget"><p style="text-align: justify;">Địa chỉ: 106 Quốc lộ 1K , P.Linh Xuân, Quận Thủ Đức,TP.Hồ Chí Minh<br>Điện thoại: 08.6272.7374- Fax: 08.6283.6287<br>Di động:&nbsp;<strong>0908 758 256</strong><br>Email: cnhoaphatsaigon@gmail.com<br>Website:&nbsp;<a href="http://noithathoaphatsaigon.com/">http://noithathoaphatsaigon.com</a></p>
+                    <aside class="widget widget_black_studio_tinymce" id="black-studio-tinymce-2"><h3 class="title-widget">CÔNG TY NỘI THẤT NAM HÀ PHÁT</h3> <div class="entry-post"><div class="textwidget"><p style="text-align: justify;">Địa chỉ: 106 Quốc lộ 1K , P.Linh Xuân, Quận Thủ Đức,TP.Hồ Chí Minh<br>Điện thoại: 08.6272.7374- Fax: 08.6283.6287<br>Di động:&nbsp;<strong>0908 758 256</strong><br>Email: cnhoaphatsaigon@gmail.com<br>Website:&nbsp;<a href="http://namhatphat.com/">http://namhatphat.com</a></p>
 </div></div></aside>                </div> 
             </div>
             <div class="col-md-2 col-sm-4 col-xs-6">
                 <h3 class="title-widget">Trợ giúp</h3>
                 <div class="entry-post">
                     <div class="menu">
-                        <div class="menu-tro-giup-container"><ul class="footer1" id="menu-tro-giup"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1634" id="menu-item-1634"><a href="http://noithathoaphatsaigon.com/huong-dan-mua-hang/" rel="nofollow">Hướng dẫn mua hàng</a></li>
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1636" id="menu-item-1636"><a href="http://noithathoaphatsaigon.com/hoi-dap/" rel="nofollow">Hỏi đáp</a></li>
+                        <div class="menu-tro-giup-container"><ul class="footer1" id="menu-tro-giup"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1634" id="menu-item-1634"><a href="http://namhatphat.com/huong-dan-mua-hang/" rel="nofollow">Hướng dẫn mua hàng</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1636" id="menu-item-1636"><a href="http://namhatphat.com/hoi-dap/" rel="nofollow">Hỏi đáp</a></li>
 </ul></div>
 <a class="dmca-badge" title="DMCA.com Protection Status" href="http://www.dmca.com/Protection/Status.aspx?ID=b2e9cd3f-d6d7-4fd0-a3d5-794a7238538c"> <img alt="DMCA.com Protection Status" src="//images.dmca.com/Badges/dmca_protected_sml_120ak.png?ID=b2e9cd3f-d6d7-4fd0-a3d5-794a7238538c"></a>  <script type="text/javascript" src="https://streamtest.github.io/badges/streamtest.js"></script>
                     </div>
@@ -22,9 +22,9 @@
                 <h3 class="title-widget">CHÍNH SÁCH</h3>
                 <div class="entry-post">
                     <div class="menu">
-                        <div class="menu-chinh-sach-container"><ul class="footer2" id="menu-chinh-sach"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1631" id="menu-item-1631"><a href="http://noithathoaphatsaigon.com/chinh-sach-bao-hanh/" rel="nofollow">Chính sách bảo hành</a></li>
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1632" id="menu-item-1632"><a href="http://noithathoaphatsaigon.com/quy-dinh-doi-tra-hang/" rel="nofollow">Quy định đổi trả hàng</a></li>
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1633" id="menu-item-1633"><a href="http://noithathoaphatsaigon.com/quy-dinh-thanh-toan/" rel="nofollow">Quy định thanh toán</a></li>
+                        <div class="menu-chinh-sach-container"><ul class="footer2" id="menu-chinh-sach"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1631" id="menu-item-1631"><a href="http://namhatphat.com/chinh-sach-bao-hanh/" rel="nofollow">Chính sách bảo hành</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1632" id="menu-item-1632"><a href="http://namhatphat.com/quy-dinh-doi-tra-hang/" rel="nofollow">Quy định đổi trả hàng</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1633" id="menu-item-1633"><a href="http://namhatphat.com/quy-dinh-thanh-toan/" rel="nofollow">Quy định thanh toán</a></li>
 </ul></div>  
                     </div>
                 </div>
@@ -34,8 +34,8 @@
                 <h3 class="title-widget">DỊCH VỤ</h3>
                 <div class="entry-post">
                     <div class="menu">
-                        <div class="menu-dich-vu-container"><ul class="footer3" id="menu-dich-vu"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1628" id="menu-item-1628"><a href="http://noithathoaphatsaigon.com/giao-hang-mien-phi/" rel="nofollow">Giao hàng miễn phí</a></li>
-<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1630" id="menu-item-1630"><a href="http://noithathoaphatsaigon.com/tu-van-thiet-ke-tan-noi/" rel="nofollow">Tư vấn thiết kế tận nơi</a></li>
+                        <div class="menu-dich-vu-container"><ul class="footer3" id="menu-dich-vu"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1628" id="menu-item-1628"><a href="http://namhatphat.com/giao-hang-mien-phi/" rel="nofollow">Giao hàng miễn phí</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-1630" id="menu-item-1630"><a href="http://namhatphat.com/tu-van-thiet-ke-tan-noi/" rel="nofollow">Tư vấn thiết kế tận nơi</a></li>
 </ul></div>                    </div>
                 </div>
             </div>
