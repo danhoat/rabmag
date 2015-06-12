c96dd688d177bddd1deb5a1e7f3ec92c2b43b18b
danngyw <danng@youngworld.vn>
2015-06-12 11:02:06 +0700

    style h3

--- template/block-partner.php
+++ template/block-partner.php
@@ -5,8 +5,10 @@
 
 	// The Loop
 	if ( have_posts() ):
-		echo '<div class="row block-partner">';
-		echo '<h3 class="title main-title">'.__('My Parner',RAB_DOMAIN).'</h2>';
+		echo '<div class ="row block-partner">';
+		echo '<div class ="col-lg-12">';
+		echo '<h3 class ="title main-title">'.__('My Parner',RAB_DOMAIN).'</h2>';
+		echo '</div>';
 		while ( have_posts() ) : the_post();
 		    echo ' <div class="col-md-3 portfolio-item">';
 		    //the_title();
