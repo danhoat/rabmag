fb785d41c394c416ae805a201520523056c4e227
danngyw <danng@youngworld.vn>
2015-06-13 22:23:01 +0700

    update

---
+++ inc/widgets/post-slider.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * display list post_type service.
+ */
+
+	/**
+ * Adds Foo_Widget widget.
+ */
+class RAB_Slider_Widget extends WP_Widget {
+	protected $list_slider = null;
+	function __construct() {
+
+		$this->list_slider = RAB_Option::get_slider();
+
+		$widget_ops = array('classname' => 'rab_slider', 'description' => __( 'A list of your site&#8217;s Pages.',RAB_DOMAIN) );
+		parent::__construct('slider', __('Rap slider 1',RAB_DOMAIN), $widget_ops);
+
+		//add_action('wp_footer',array($this,'add_flex_script_footer'));
+	}
+	function add_flex_script_footer(){
+
+		$args = $this->get_settings();
+		$options = get_option('widget_slider',array());
+		unset($options['_multiwidget']);
+		if($options){
+			?>
+			<script type = "text/javascript">
+				(function($){
+
+					$(document).ready(function(){
+						<?php
+							foreach ($options as $key => $widget) {
+								extract($widget);
+								$nav = (isset($nav) && $nav==1) ? 'true' : 'false';
+
+						 		$class = '.slider-'.$key; ?>
+						 		$('<?php echo $class;?>').flexslider({
+					              	animation: '<?php echo $effect;?>',
+					              	animationSpeed:<?php echo $widget['speed'];?>,
+					              	animationLoop: true,
+					              	itemWidth: '100%',
+					              	slideshowSpeed : 5000,
+					              	itemMargin: 0,
+					                controlNav: <?php echo $nav;?>,
+					                slideshow: true,
+
+					            });
+				            <?php
+							}
+						?>
+					});
+				})(jQuery);
+			</script>
+			<?php
+		}
+	}
+
+	function widget( $args, $instance ) {
+
+		wp_enqueue_script('jquery.flexslider');
+		wp_enqueue_style('flex.slider');
+		wp_enqueue_style('flex.demo');
+		extract( $args );
+
+		echo $before_widget;
+
+		extract($instance);
+
+		?>
+
+		<div class="flexslider carousel <?php echo $widget_id;?>">
+			<?php
+			if($title)
+				echo $before_title .' &nbsp;'.$title. $after_title;
+				$query = 'post_type=post';
+				if(is_numeric($cat))
+					$query.='&cat='.$cat;
+
+
+				query_posts($query);
+				if(have_posts()){
+					echo '<ul class="slides">';
+					while(have_posts()){
+						the_post();
+						echo '<li>';
+						the_title();
+						rab_post_thumbnail();
+						echo'</li>';
+					}
+					echo '</ul>';
+				}
+				wp_reset_query();
+				?>
+
+		</div>
+
+		<?php
+			echo $after_widget;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		return $new_instance;
+	}
+
+	function form( $instance ) {
+		//Defaults
+		$instance = wp_parse_args( (array) $instance , array( 'speed' => 400, 'title' => '', 'nav' =>0 , 'effect' => 'slide', 'slide' => '0','size' => 'full') );
+		$title = esc_attr( $instance['title'] );
+		extract($instance);
+
+	?>
+		<p>
+			<label for="<?php echo $this->get_field_id('cat'); ?>"><?php _e('Select Category',RAB_DOMAIN); ?>:</label>
+			<select name="<?php echo $this->get_field_name("cat");?>">
+				<?php
+
+				if(is_array($this->list_slider) && !empty($this->list_slider)){
+
+					echo '<option value="0">'.__('Selected Slider',RAB_DOMAIN).'</option>';
+					foreach ($this->list_slider as $key => $title) {?>
+
+						<option value="<?php echo $key;?>" <?php selected($key,$slide);?> > <?php echo $title;?> </option>
+						<?php
+					}
+				} else {
+
+					echo '<option value="0">'.__('No slider',RAB_DOMAIN).'</option>';
+				}
+				?>
+			</select>
+
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('effect'); ?>"><?php _e('Select type effect',RAB_DOMAIN);?> : </label>
+			<?php
+				$arg= array('name' => 'cat','selected' => 0);
+				wp_dropdown_categories( $args );
+			?>
+
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('size'); ?>"><?php _e('Select thumbnail size',RAB_DOMAIN);?> : </label>
+			<select name="<?php echo $this->get_field_name("size");?>"> 
+
+				<option value="full" <?php selected('full',$size);?> > <?php _e('Full',RAB_DOMAIN);?></option>
+				<option value="thumbnail" <?php selected('thumbnail',$size);?> > <?php _e('Thumbnail',RAB_DOMAIN);?></option>
+
+			</select>
+
+		</p>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title',RAB_DOMAIN); ?> : </label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Speed:',RAB_DOMAIN); ?> : </label> 
+			<input id="<?php echo $this->get_field_id('speed'); ?>" name="<?php echo $this->get_field_name('speed'); ?>" type="text" value="<?php echo $speed; ?>" class="widefat" />
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('nav'); ?>"><?php _e('Show Nav',RAB_DOMAIN); ?>: </label> 
+			<input <?php if($nav ==1) echo 'checked ="checked"';?> id="<?php echo $this->get_field_id('nav'); ?>" name="<?php echo $this->get_field_name('nav'); ?>" type="checkbox" value="1"  />
+		</p>
+
+<?php
+	}
+
+}
+?>
