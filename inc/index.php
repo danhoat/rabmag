5ce2c9b03217a842f256d22f634222fd79be2fc5
danngyw <danng@youngworld.vn>
2015-06-17 23:16:27 +0700

    update

--- inc/index.php
+++ inc/index.php
@@ -3,6 +3,7 @@ require get_template_directory() . '/inc/comments.php';
 require get_template_directory() . '/inc/widgets.php';
 require get_template_directory() . '/inc/widgets/index.php';
 require get_template_directory() . '/inc/themes.php';
+require get_template_directory() . '/inc/customizer.php';
 
 
 ?>
