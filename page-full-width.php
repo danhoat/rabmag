952066f573b75258ad304a51fff4195138ad467e
danngyw <danng@youngworld.vn>
2015-06-20 20:19:14 +0700

    update

---
+++ page-full-width.php
@@ -0,0 +1,37 @@
+<?php
+  /**
+  * Template Name: Full Width
+  */
+?>
+<?php get_header(); ?>
+
+<div class="container main-page">
+    <div class="row">
+
+            <div class="entry-page">
+
+                <?php
+
+                do_action("rab_before_loop");
+
+                if(have_posts()):
+                    echo '<h1 class="title">'.get_the_title().'</h1>';
+                    the_post();
+                    echo '<div class="content">';
+                    the_content();
+                    echo '</div>';
+                else :
+                    get_template_part('template/none' );
+
+                endif;
+
+                ?>
+                <?php do_action("rab_after_loop") ?>
+
+            </div> <!-- .endtry end !-->
+
+    </div> <!-- .row !-->
+
+</div> <!-- End main-content!-->
+
+<?php get_footer();?>
